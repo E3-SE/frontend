72aa62388676e5bc861b6b3731860bca14bf1b63
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2026-03-20 21:04:50 +0700

    Add image url to next config

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,11 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "api.dicebear.com",
         pathname: "/9.x/lorelei/svg",
+      },
+      {
+        protocol: "https",
+        hostname: "img.rachatat.com",
+        pathname: "/**",
       }
     ],
   },
