f013d3c0df1c3216f295ec39b2aa000a6c968c38
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2026-04-17 20:13:00 +0700

    Update apiBaseUrl to support different environments

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,1 +1,1 @@
-export const apiBaseUrl = "https://e3-backend.vercel.app";
+export const apiBaseUrl = process.env.NODE_ENV === "production" ? "https://e3-backend.vercel.app" : "https://e3-backend-dev.vercel.app";
